b9b403dd43790daebecfe2062a4d80d954014a63
Camilo Lopez <camilo.lopez@shopify.com>
2012-07-05 00:11:49 -0400

    WIP general clean up

--- lib/dimsum.rb
+++ lib/dimsum.rb
@@ -1,5 +1,38 @@
 require "dimsum/version"
 
 module Dimsum
-  # Your code goes here...
+  def reservoir_file(filename, sample_size)
+    random = Random.new
+    out = []
+
+    File.open(filename, "r").each_line.each_with_index do |line, line_number|
+      keep, index = keep_line_in_index(line_number, sample_size, random)
+      out[index] = line if keep
+    end
+    out
+  end
+
+  def reservoir_stdin(sample_size)
+    random = Random.new
+    input_line_number = 0
+    out = []
+
+    STDIN.readlines.each do |line|
+      keep, index = keep_line_in_index(input_line_number, sample_size,  random)
+      out[index] = line if keep
+      input_line_number += 1
+    end
+
+    out
+  end
+
+  def keep_line_in_index(input_line_number, sample_size, random_number_generator)
+    if input_line_number < sample_size
+      [true, input_line_number]
+    else
+      r = random_number_generator.rand(0 .. input_line_number)
+      keep = r < sample_size 
+      [keep, r]
+    end
+  end
 end
---
+++ test/dimsum_test.rb
@@ -0,0 +1,34 @@
+require 'lib/dimsum'
+require 'minitest/autorun'
+
+class TestDimsum < MiniTest::Unit::TestCase 
+  include Dimsum
+
+  def test_keep_line_in_index__should_keep_any_line_number_less_than_sample_size
+    random = MiniTest::Mock.new
+    sample_size = 5
+
+    (0..4).each do |k|
+      keep, _ = keep_line_in_index(k, sample_size, random)
+      assert keep
+    end
+  end
+
+  def test_keep_line_inde_index__should__keep_line_when_index_is_less_than_random
+    expected_index = 2
+    random = MiniTest::Mock.new
+    current_line = 10
+    random.expect :rand, expected_index, (0 .. current_line)
+
+    sample_size = 5
+
+    keep, actual_index = keep_line_in_index(10, sample_size, random)
+    assert keep
+    assert_equal expected_index, actual_index
+  end
+
+  def test_keep_line_inde_index__should_not_keep_line_when_index_is_more_or_equal_than_random
+    flunk
+  end
+end
+
